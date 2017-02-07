b7eca5b30ed0faf8b071d6cdf11056cbd9a7f54c
wenbing <dongwenbingxc@gmail.com>
2017-02-07 09:45:47 +0800

    年后第一更

--- dev/controller/nvcontroller.js
+++ dev/controller/nvcontroller.js
@@ -7,18 +7,28 @@
           items: null,
           province: null,
           city: null,
-          area: null
+          area: null,
+          scotop: 0
       },
       methods: {
           getdata: function() {
               this.$http.get("/dev/data/nv/province.json").then(function(rtdata) {
-                  if (!rtdata) return false;
-                  rtdata = eval("(" + rtdata.body + ")");
-                  this.items = rtdata;
-              }, function(error) {})
+                      if (!rtdata) return false;
+                      rtdata = eval("(" + rtdata.body + ")");
+                      this.items = rtdata;
+                  },
+                  function(error) {})
+          },
+
+          pickscorll: function(event) {
+              this.scotop = event.target.scrollTop;
+              var itemheight = event.target.scrollHeight / (this.items.length + 2)
+
+              //   alert("" + this.items[6].ProID + "");
+              debugger
+              var bb = 55;
           }
       },
   })
-
 
   vm.getdata();
