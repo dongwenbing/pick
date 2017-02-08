7151d504b257dbadec2970d2b8e14042297fc7d7
wenbing <dongwenbingxc@gmail.com>
2017-02-08 15:50:26 +0800

    选择效果

--- dev/controller/nvcontroller.js
+++ dev/controller/nvcontroller.js
@@ -1,82 +1,34 @@
-    function aaa(aaa) {
-        alert("4566788");
+  var vm = new Vue({
+      el: '#selectCity',
+      data: {
+          provincedefual: "请选择...",
+          citydefual: "请选择...",
+          area: "请选择...",
+          items: null,
+          province: null,
+          city: null,
+          area: null,
+          scotop: 0
+      },
+      methods: {
+          getdata: function() {
+              this.$http.get("/dev/data/nv/province.json").then(function(rtdata) {
+                      if (!rtdata) return false;
+                      rtdata = eval("(" + rtdata.body + ")");
+                      this.items = rtdata;
+                  },
+                  function(error) {})
+          },
 
-        debugger
-        var e = 6;
-    }
+          pickscorll: function(event) {
+              this.scotop = event.target.scrollTop;
+              var itemheight = event.target.scrollHeight / (this.items.length + 2)
 
-    var vm = new Vue({
-        el: '#selectCity',
-        data: {
-            provincedefual: "请选择...",
-            citydefual: "请选择...",
-            area: "请选择...",
-            items: null,
-            province: null,
-            city: null,
-            area: null,
-            scotop: 0,
-            pickstate: 5,
-            pickitemName: "",
-            pickitemId: null,
-            pickcallback: function() {
-                alert("666");
-            }
-        },
-        methods: {
-            getdata: function() {
-                this.$http.get("/dev/data/nv/province.json").then(function(rtdata) {
-                        if (!rtdata) return false;
-                        rtdata = eval("(" + rtdata.body + ")");
-                        this.items = rtdata;
-                    },
-                    function(error) {})
-            },
-            pickclick: function(event, aaa) {
-                this.pickitemName = event.target.innerText;
-                this.pickitemId = event.target.attributes.itemid.value;
-                this.pickstate = event.target.attributes.itemid.value;
-                // this.pickitemId ? this.pickcallback() : null;
+              //   alert("" + this.items[6].ProID + "");
+              debugger
+              var bb = 55;
+          }
+      },
+  })
 
-                this.scotop = event.target.parentNode.scrollTop;
-                var itemheight = event.target.parentNode.scrollHeight / (this.items.length + 2); //这种高度暂时可不用
-                var itemlist = event.target.parentNode.children;
-                itemheight = event.target.offsetHeight;
-                var YY = Math.ceil(this.scotop / itemheight); //向上取整
-                if (YY > 0) {
-                    var setheight = YY * itemheight;
-                    setheight -= itemheight / 2;
-                    setheight ? event.target.parentNode.scrollTop = setheight : null;
-                }
-                // debugger
-                // var bb = 55;
-
-                // var newclass = document.getElementsByClassName("pick-item-se");该方法已抛弃
-                // for (i in newclass) { newclass[i].className = "pick-item"; }
-
-                //   event.target.className = "pick-items";
-                event.target.className = "pick-item-se";
-                //  debugger
-                var bb = 55;
-
-
-            },
-
-            pickscorll: function(event) {
-                this.scotop = event.target.scrollTop;
-                var itemheight = event.target.scrollHeight / (this.items.length + 2); //这种高度暂时可不用
-                var itemlist = event.target.children;
-                if (event.target.children[0]) { itemheight = event.target.children[0].offsetHeight; }
-                var YY = Math.ceil(this.scotop / itemheight); //向上取整
-                if (YY > 0) {
-                    var setheight = YY * itemheight;
-                    setheight -= itemheight / 2;
-                    setheight ? event.target.scrollTop = setheight : null;
-                }
-                debugger
-                var bb = 55;
-            }
-        },
-    })
-
-    vm.getdata();
+  vm.getdata();
